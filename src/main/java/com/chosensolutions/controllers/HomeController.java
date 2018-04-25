a1a8b131fa2107c1dc995e9277c4edc7804372fa
yichen <yichenzhu1337@gmail.com>
2018-04-24 22:18:09 -0400

    new spring boot init

--- src/main/java/com/chosensolutions/controllers/HomeController.java
+++ src/main/java/com/chosensolutions/controllers/HomeController.java
@@ -10,4 +10,9 @@ public class HomeController {
     public String home() {
         return "Hello world";
     }
+
+    @RequestMapping("/test")
+    public String test() {
+        return "Test Charles is cool";
+    }
 }
