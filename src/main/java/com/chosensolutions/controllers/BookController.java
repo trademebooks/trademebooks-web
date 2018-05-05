dd6249d319fe8158d55f91010fd95288cbf5b658
yichenzhu1337 <yichenzhu1337@gmail.com>
2018-05-04 21:05:14 -0400

    New updates!

--- src/main/java/com/chosensolutions/controllers/BookController.java
+++ src/main/java/com/chosensolutions/controllers/BookController.java
@@ -1,14 +1,28 @@
 package com.chosensolutions.controllers;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class BookController {
 
-    @GetMapping("/books")
-    public String books() {
+    @RequestMapping("/books")
+    public String index() {
+        return "books/books.index";
+    }
 
-        return "books";
+    @RequestMapping("/books/1")
+    public String show() {
+        return "books/books.show";
+    }
+
+    @RequestMapping("/books/create")
+    public String create() {
+        return "books/books.create";
+    }
+
+    @RequestMapping("/books/edit")
+    public String edit() {
+        return "books/books.edit";
     }
 }
---
+++ src/main/resources/templates/books/books.index.html
@@ -0,0 +1,1 @@
+<h1>Books Index</h1>
