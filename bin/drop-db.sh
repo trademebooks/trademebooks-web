d65909b3535f18baf32f6073edfa327c767b234b
yichenzhu1337 <yichenzhu1337@gmail.com>
2018-05-09 00:47:00 -0400

    New updates!

--- bin/drop-db.sh
+++ bin/drop-db.sh
@@ -2,7 +2,7 @@
 
 targeted_database=book_exchange_please
 
-mysql -u root -p --default-character-set=utf8 -e "
+mysql -u root -p"" --default-character-set=utf8 -e "
 DROP DATABASE IF EXISTS $targeted_database;
 CREATE DATABASE $targeted_database;
 USE $targeted_database;
