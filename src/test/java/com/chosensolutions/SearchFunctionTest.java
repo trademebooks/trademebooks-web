b6babe2845c41549be96ffa9c68d3334d70643a4
yichen <yichenzhu1337@gmail.com>
2018-05-29 18:46:41 -0400

    SearchFunctionTest

--- src/test/java/com/chosensolutions/SearchFunctionTest.java
+++ src/test/java/com/chosensolutions/SearchFunctionTest.java
@@ -1,17 +1,39 @@
 package com.chosensolutions;
 
-import com.chosensolutions.controllers.PagesController;
+import com.chosensolutions.utils.HTTP;
+import com.google.gson.*;
 import org.junit.Test;
-
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 public class SearchFunctionTest {
 
     @Test
-    public void testApp() {
+    public void HTTPGet() throws Exception {
+        String url = "https://www.googleapis.com/books/v1/volumes?q=calculus%20spivak";
+        String response = HTTP.get(url);
+        HTTP.prettyPrint(response);
+        assertTrue(response.length() > 0);
+    }
 
-        PagesController pagesController = new PagesController();
-        String aboutPage = pagesController.about();
-        assertEquals(aboutPage, "pages/about");
+    @Test
+    public void getJsonDataFromGoogleBooksAPI() throws Exception {
+        String url = "https://www.googleapis.com/books/v1/volumes?q=calculus%20spivak";
+        String response = HTTP.get(url);
+
+        JsonElement jelement = new JsonParser().parse(response);
+        JsonObject jsonObject = jelement.getAsJsonObject();
+
+        JsonPrimitive totalItems = jsonObject.getAsJsonPrimitive("totalItems");
+        JsonArray items = jsonObject.getAsJsonArray("items");
+        System.out.println(totalItems.getAsInt());
+        System.out.println(items);
+
+        System.out.println(jsonObject);
+
+        for (JsonElement item: items) {
+            System.out.println(item.getAsJsonObject().getAsJsonPrimitive("id"));
+        }
+
+        assertTrue(totalItems.isNumber());
     }
 }
