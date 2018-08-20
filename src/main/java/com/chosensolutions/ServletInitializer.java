d10526e6bd4d954a24f00633fa83f09402afd498
yichenzhu1337 <yichenzhu1337@gmail.com>
2018-08-20 16:22:44 -0400

    Made some new updates to the repo.

---
+++ src/main/java/com/chosensolutions/ServletInitializer.java
@@ -0,0 +1,15 @@
+package com.chosensolutions;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+/**
+ * This allows use to run a SpringApplication from a traditional WAR archive
+ */
+public class ServletInitializer extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(App.class);
+    }
+} 
