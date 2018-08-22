2dbdd0444ab0abed5f8c42b5731f3c6eeef839dd
yichenzhu1337 <yichenzhu1337@gmail.com>
2018-08-22 05:13:50 -0400

    Made some new updates to the repo.

---
+++ src/main/java/com/chosensolutions/controllers/AccountController.java
@@ -0,0 +1,29 @@
+package com.chosensolutions.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class PagesController {
+
+    @RequestMapping("/")
+    public String home() {
+        return "pages/home";
+    }
+
+    @RequestMapping("/about")
+    public String about() {
+        return "pages/about";
+    }
+
+    @RequestMapping("/contact")
+    public String contact() {
+        return "pages/contact";
+    }
+
+    @RequestMapping("/terms")
+    public String terms() {
+        return "pages/terms";
+    }
+
+}
---
+++ src/main/java/com/chosensolutions/controllers/DashboardController.java
@@ -0,0 +1,14 @@
+package com.chosensolutions.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class AccountController {
+
+    @RequestMapping("/account/edit")
+    public String accountEditPage() {
+        return "account/account.edit";
+    }
+
+}
