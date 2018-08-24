2eaa1ad584023e9290e6c0ec360cdb38e37e624d
yichenzhu1337 <yichenzhu1337@gmail.com>
2018-08-24 17:55:41 -0400

    Made some new updates to the repo.

--- src/main/resources/static/js/book/book.create.js
+++ src/main/resources/static/js/book/book.create.js
@@ -1,40 +1,51 @@
-jQuery(document).ready(function ($) {
-    // Set the Options for "Bloodhound" suggestion engine
-    var engine = new Bloodhound({
-        remote: {
-            url: 'https://www.googleapis.com/books/v1/volumes?q=%QUERY%',
-            wildcard: '%QUERY%',
-            transform: function (response) {
-                return response.items;
+(function ($) {
+    $(document).ready(function ($) {
+        // Set the Options for "Bloodhound" suggestion engine
+        var engine = new Bloodhound({
+            remote: {
+                url: 'https://www.googleapis.com/books/v1/volumes?q=%QUERY%',
+                wildcard: '%QUERY%',
+                transform: function (response) {
+                    return response.items;
+                }
+            },
+            datumTokenizer: Bloodhound.tokenizers.whitespace('q'),
+            queryTokenizer: Bloodhound.tokenizers.whitespace
+        });
+        $(".search-input").typeahead({
+            hint: true,
+            highlight: true,
+            minLength: 1
+        }, {
+            source: engine.ttAdapter(),
+            // This will be appended to "tt-dataset-" to form the class name of the suggestion menu.
+            name: 'items',
+            display: 'items',
+            // the key from the array we want to display (name,id,email,etc...)
+            templates: {
+                empty: [
+                    '<div class="list-group search-results-dropdown">',
+                        '<div class="list-group-item">Nothing found.</div>',
+                    '</div>'
+                ].join('\n'),
+                header: [
+                    '<div class="list-group search-results-dropdown">'
+                ],
+
+                suggestion: function (data) {
+                    return [
+                        '<div>',
+                            '<img width="50" src="' + data.volumeInfo.imageLinks.thumbnail + '"/> ',
+                            data.volumeInfo.title + ' - ' + data.volumeInfo.authors[0],
+                        '</div>'
+                    ].join('\n');
+                }
             }
-        },
-        datumTokenizer: Bloodhound.tokenizers.whitespace('q'),
-        queryTokenizer: Bloodhound.tokenizers.whitespace,
+        });
+        
+        $('.search-input').on('typeahead:select', function (event, suggestion) {
+            console.log(event);
+            console.log(suggestion);
+        });
     });
-
-    $(".search-input").typeahead({
-        hint: true,
-        highlight: true,
-        minLength: 1
-    }, {
-        source: engine.ttAdapter(),
-
-        // This will be appended to "tt-dataset-" to form the class name of the suggestion menu.
-        name: 'items',
-
-        display: 'items',
-
-        // the key from the array we want to display (name,id,email,etc...)
-        templates: {
-            empty: [
-                '<div class="list-group search-results-dropdown"><div class="list-group-item">Nothing found.</div></div>'
-            ],
-            header: [
-                '<div class="list-group search-results-dropdown">'
-            ],
-            suggestion: function (data) {
-                return '<div><a href="' + data.id + '" class="list-group-item">' + data.volumeInfo.title + ' - ' + data.volumeInfo.authors[0] + '<img src="' + data.volumeInfo.imageLinks.thumbnail + '"/> ' + '</a></div>'
-            }
-        }
-    });
-});
+})(jQuery);
--- src/test/java/com/chosensolutions/BookServiceTest.java
+++ src/test/java/com/chosensolutions/BookServiceTest.java
@@ -9,10 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import java.util.List;
-import java.util.Set;
-
-import static org.junit.Assert.assertEquals;
+import java.util.*;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
