5946d7ed37d6b4d5a036e82ff5ab61e17fb4265d
Yi Chen Zhu <yichenzhu1337@gmail.com>
2019-04-26 16:07:54 -0400

    New updates!

---
+++ client/src/components/ComponentA.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
+
+class MyComponent extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+MyComponent.propTypes = {};
+
+export default MyComponent;
---
+++ client/src/components/Post.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+class Post extends Component {
+    render() {
+        return (
+            <div>
+
+            </div>
+        );
+    }
+}
+
+export default Post;
+
