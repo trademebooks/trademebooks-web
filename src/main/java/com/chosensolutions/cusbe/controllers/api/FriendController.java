a1928e5e55b90d075660fc92f3023835043ed5ac
yichen <yichenzhu1337@gmail.com>
2019-06-13 23:10:00 -0400

    New updates!

--- src/main/java/com/chosensolutions/cusbe/controllers/api/FriendController.java
+++ src/main/java/com/chosensolutions/cusbe/controllers/api/FriendController.java
@@ -1,12 +1,10 @@
 package com.chosensolutions.cusbe.controllers.api;
 
 import com.chosensolutions.cusbe.domain.dto.FriendDto;
-import com.chosensolutions.cusbe.repositories.FriendRepository;
+import com.chosensolutions.cusbe.services.friend.FriendService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import java.math.*;
-import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -14,23 +12,11 @@ import java.util.List;
 public class FriendController {
 
     @Autowired
-    private FriendRepository friendRepository;
+    FriendService friendService;
 
     @GetMapping("/all")
     public List<FriendDto> friends() {
-        List<Object[]> friends = friendRepository.queryGetAllFriendsOfUserThroughId("1");
-        List<FriendDto> friendDtos = new ArrayList<>();
-        for (Object[] friend : friends) {
-            FriendDto friendDto = new FriendDto();
-
-            friendDto.setUserId((BigInteger) friend[0]);
-            friendDto.setEmail((String) friend[1]);
-            friendDto.setFirstName(String.valueOf(friend[2]));
-            friendDto.setLastName((String) friend[3]);
-            friendDtos.add(friendDto);
-        }
-
-        return friendDtos;
+        return friendService.getAllFriendsByUserId();
     }
 
 }
