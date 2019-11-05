4a53685740ada0b62ee1de6f5c575d3803d59306
Yi Chen Zhu <yichenzhu1337@gmail.com>
2019-11-05 15:11:24 -0500

    New updates!

--- client/src/utilities/Constants.js
+++ client/src/utilities/Constants.js
@@ -5,13 +5,8 @@ export const LOGIN_URL = BASE_URL + 'login';
 export const LOGOUT_URL = BASE_URL + 'logout';
 export const USER_PROFILE_URL = BASE_URL + 'user/profile';
 
-export const DRAFT_CREATE_URL = BASE_URL + 'draft';
-export const DRAFT_GET_PENDING_URL = BASE_URL + 'draft/pending';
-export const DRAFT_GET_IN_PROGRESS_URL = BASE_URL + 'draft/in-progress';
-export const DRAFT_GET_COMPLETED_URL = BASE_URL + 'draft/completed';
-export const DRAFT_GET_ONE_URL = BASE_URL + 'draft';
-export const DRAFT_GET_CREATE_URL = BASE_URL + 'draft';
-export const ROSTER_GET_ROSTERS_URL = BASE_URL + 'roster';
-export const ROSTER_PUT_SUBMIT_ROSTER_FOR_PENDING_DRAFT_URL = BASE_URL + 'draft/';
-
 export const GET_ALL_PUBLIC_BOOKS_URL = BASE_URL + 'books';
+export const GET_ONE_BOOK_URL = BASE_URL + 'books/';
+export const CREATE_A_Book_URL = BASE_URL + 'books';
+export const UPDATE_A_BOOK_URL = BASE_URL + 'books/';
+export const DELETE_A_BOOK_URL = BASE_URL + 'books/';
