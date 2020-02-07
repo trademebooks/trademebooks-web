38b12952f8922d06595148634433381cd0f03d3f
Yi Chen Zhu <yichenzhu1337@gmail.com>
2020-02-07 01:36:17 -0500

    New updates!

--- bin/push.sh
+++ bin/push.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
 
-git pull
+git pull origin master
+git pull heroku master
+
 git add -A
 git commit -m "New updates!"
+
 git push origin master
 git push heroku master # https://git.heroku.com/trademebooks.git
