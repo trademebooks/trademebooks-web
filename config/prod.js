3cc8ffbeca367f3d22b6a589232600d0f83b25d8
Yi Chen Zhu <yichenzhu1337@gmail.com>
2020-02-09 20:11:27 -0500

    New updates!

--- config/prod.js
+++ config/prod.js
@@ -2,7 +2,7 @@
 module.exports = {
   googleClientID: process.env.GOOGLE_CLIENT_ID,
   googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  mongoURI: process.env.MONGO_URI,
+  mongoURI: 'mongodb+srv://yichen:KEOYU2KsfsbzXdxZ@ilovetennis2-cluster-selho.mongodb.net/test?retryWrites=true&w=majority', //process.env.MONGO_URI,
   cookieKey: process.env.COOKIE_KEY,
   stripePublishableKey: process.env.STRIPE_PUBLISHABLE_KEY,
   stripeSecretKey: process.env.STRIPE_SECRET_KEY,
