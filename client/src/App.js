a53fb9d041ad8d708ac765e3caf4794122f0bd0b
Yi Chen Zhu <yichenzhu1337@gmail.com>
2020-03-12 15:05:40 -0400

    redux change in app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,5 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
-import PropTypes from "prop-types";
 import {BrowserRouter, Route, Switch} from 'react-router-dom';
 
 import './css/App.css';
@@ -66,6 +65,6 @@ function mapDispatchToProps(dispatch) {
 }
 
 export default connect(
-    null,//mapStateToProps,
-    actions //mapDispatchToProps
+    mapStateToProps,
+    mapDispatchToProps
 )(App);
