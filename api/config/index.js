c3bfd77717bec002f3b2953c34f944f2e1c85139
Yichen Zhu <yichenzhu1337@gmail.com>
2020-08-24 19:18:07 -0400

    fix: heroku deploy test

--- api/config/index.js
+++ api/config/index.js
@@ -3,5 +3,8 @@ let config = require('./dev');
 if (process.env.NODE_ENV === 'testing') {
   config = require('./testing');
 }
+else if (process.env.NODE_ENV === 'production') {
+  config = require('./production');
+}
 
 module.exports = config;
--- server.js
+++ server.js
@@ -3,23 +3,22 @@ const PORT = process.env.PORT || 5000;
 
 const db = require('./api/utils/db');
 
-// Serve static assets in production
+// Frontend - Serve static assets in production
 if (process.env.NODE_ENV === 'production') {
-  // Set static folder
   app.use(express.static('client/build'));
-
   const path = require('path');
-
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
 }
 
+// Backend - Server
 server.listen(PORT, async () => {
   await db(); // start the database
   console.log(`Visit the API's health check page at: http://localhost:${PORT}/api/v1/app/health`);
 })
 
+// global exception handler
 // process.on("unhandledRejection", error => {
 //   console.log(error.message);
 //   console.log("--------------------------");
