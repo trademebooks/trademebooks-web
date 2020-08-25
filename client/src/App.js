12bd1e75171724d92e08e928018b41bd2915bdd1
Yichen Zhu <yichenzhu1337@gmail.com>
2020-08-25 15:38:06 -0400

    The world changes very quickly. The Earth will continue to rotate whether you like it or not. -Yichen Zhu

--- client/src/App.js
+++ client/src/App.js
@@ -10,9 +10,9 @@ import store from './store';
 import { loadUser } from './actions/auth';
 
 // mdbootstrap CSS
-import "@fortawesome/fontawesome-free/css/all.min.css";
-import "bootstrap-css-only/css/bootstrap.min.css";
-import "mdbreact/dist/css/mdb.css";
+import '@fortawesome/fontawesome-free/css/all.min.css';
+import 'bootstrap-css-only/css/bootstrap.min.css';
+import 'mdbreact/dist/css/mdb.css';
 // global CSS
 import './css/global.scss';
 
