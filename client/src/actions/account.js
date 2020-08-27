14dd8677b16f78a476e8c4ab82200af2fa68d39a
HarrisonFok <pak.fok@mail.utoronto.ca>
2020-08-27 16:32:29 -0400

    Added types for updating account; added action for updating emails; will now do the frontend for email and then connect to the backend

---
+++ client/src/actions/account.js
@@ -0,0 +1,25 @@
+import api from "../utils/api";
+import { setAlert } from "./alert";
+import {
+  UPDATE_EMAIL,
+  UPDATE_PASSWORD,
+  UPDATE_BOOKSTORE,
+  UPDATE_NOTIFICATIONS,
+  UPDATE_ERROR
+} from "./types";
+
+export const updateEmail = (userId, email) => dispatch => {
+    try {
+        const res = await api.get(`/profile/user/${userId}`);
+
+        dispatch({
+            type: UPDATE_EMAIL,
+            payload: res.data
+          });
+    } catch (err) {
+        dispatch({
+            type: UPDATE_ERROR,
+            payload: { msg: err.response.statusText, status: err.response.status }
+        })
+    }
+}
