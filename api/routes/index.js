a3d08fae208be87485317bc7d6850901868dcb38
HarrisonFok <pak.fok@mail.utoronto.ca>
2020-09-09 23:56:09 -0400

    Fixed the bug where the endpoint is not found

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,16 +1,17 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-const authRoutes = require('./auth.route');
-const bookRoutes = require('./book.route');
-const appRoutes = require('./app.route');
+const authRoutes = require("./auth.route");
+const bookRoutes = require("./book.route");
+const appRoutes = require("./app.route");
+const bookstoreRoutes = require("./bookstore.route");
 
 function getRouter() {
-  router.use('/auth', authRoutes);
-  router.use('/books', bookRoutes);
-  router.use('/bookstores', bookRoutes);
-  
-  router.use('/app', appRoutes);
+  router.use("/auth", authRoutes);
+  router.use("/books", bookRoutes);
+  router.use("/bookstores", bookstoreRoutes);
+
+  router.use("/app", appRoutes);
 
   return router;
 }
