d24d3ec76c1a95ee0e183949ace9202a91fc47b0
Yichen Zhu <yichenzhu1337@gmail.com>
2020-09-11 23:11:39 -0400

    feat: updated the post a book feature

---
+++ client/src/components/Books/add/PostBookForm/Conditions.js
@@ -0,0 +1,70 @@
+import React, { useState } from 'react';
+import { MDBRow, MDBCol, MDBContainer, MDBInput, MDBBtn } from 'mdbreact';
+
+import './Conditions.scss';
+
+const Conditions = () => {
+
+  const [condition, setCondition] = useState('');
+
+  let conditions = [
+    {
+      name: "Poor",
+      img: require('../../../../img/condition_icons/Poor_condition_default.png'),
+      desc: "No damage, lightly used, no markings",
+      imgSelected: require("../../../../img/condition_icons/Poor_condition.png")
+    },
+    {
+      name: "Fair",
+      img: require("../../../../img/condition_icons/Fair_condition_default.png"),
+      desc: "No damage, lightly used, no markings",
+      imgSelected: require("../../../../img/condition_icons/Fair_condition.png")
+    },
+    {
+      name: "Good",
+      img: require("../../../../img/condition_icons/Good_condition_default.png"),
+      desc: "No damage, lightly used, no markings",
+      imgSelected: require("../../../../img/condition_icons/Good_condition.png")
+    },
+    {
+      name: "Very Good",
+      img: require("../../../../img/condition_icons/Verygood_condition_default.png"),
+      desc: "No damage, lightly used, no markings",
+      imgSelected: require("../../../../img/condition_icons/Verygood_condition.png")
+    },
+    {
+      name: "Like New",
+      img: require("../../../../img/condition_icons/Likenew_condition_default.png"),
+      desc: "No damage, lightly used, no markings",
+      imgSelected: require("../../../../img/condition_icons/Likenew_condition.png")
+    },
+  ];
+
+  let conditionsElement = [];
+
+  // eslint-disable-next-line
+  conditions.map((cond, i) => {
+    conditionsElement.push(
+      <MDBCol md="2" sm="12" key={i} className="condition">
+        <p className="condition-title">{cond.name}</p>
+        <img className="condition-img"
+          src={condition === cond.name ? cond.imgSelected : cond.img}
+          alt={cond.name} name="condition" cond={cond.name}
+          onClick={() => setCondition(cond.name)} />
+        <p className="condition-desc">{cond.desc}</p>
+      </MDBCol>
+    )
+  });
+
+  return (
+    <>
+      <div className="mt-5">
+        <MDBRow>
+          {conditionsElement}
+        </MDBRow>
+      </div>
+    </>
+  );
+}
+
+export default Conditions;
