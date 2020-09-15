562fcff9045a2d4b55c1bb58cb0e08f0f05c89d2
Yichen Zhu <yichenzhu1337@gmail.com>
2020-09-15 18:02:17 -0400

    fix: put back books component

---
+++ client/src/components/books/common/Books.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import Book from './Book';
+
+function Books({ books }) {
+  return (
+    <>
+      <div>
+        {books.map((book, index) => {
+          return <Book key={book._id} book={book} />;
+        })}
+      </div>
+    </>
+  );
+}
+
+export default Books;
