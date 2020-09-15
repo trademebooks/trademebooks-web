918448bbe5ed80a86f816c228e8a87257e7053d0
Yichen Zhu <yichenzhu1337@gmail.com>
2020-09-15 18:28:12 -0400

    fix: changed the static serving assets code to the index.js in ./api directory

--- api/index.js
+++ api/index.js
@@ -89,4 +89,13 @@ app.use((err, req, res, next) => {
   }
 });
 
+// Frontend - Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
 module.exports = http.createServer(app);
