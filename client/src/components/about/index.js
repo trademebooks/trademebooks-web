0f9bb74074f5ade5000c74b5c094105cb36fc975
HarrisonFok <pak.fok@mail.utoronto.ca>
2020-09-27 10:41:00 -0400

    Added all the stuff in; just missing the pictures, description of each person, and designs

--- client/src/components/about/index.js
+++ client/src/components/about/index.js
@@ -1,31 +1,14 @@
 import React, { useState } from 'react';
+import { MDBRow, MDBCol, MDBContainer, MDBBtn } from 'mdbreact';
 import Person from './person';
+import Story from './story';
 
 const About = () => {
-  const names = [
-    'Yi Chen Zhu',
-    'Cedric Mosdell',
-    'Charles Petchsy',
-    'Harrison Fok',
-    'Allan C',
-    'Avi Patel',
-    'Bo Dan',
-    'David Tan',
-    'Hassan Azmi',
-    'Alex',
-    'Shuyi',
-  ];
-  const parts = names.map((name) => {
-    return (
-      <div>
-        <Person name={name} />
-      </div>
-    );
-  });
   return (
     <div>
       <h1>About</h1>
-      <div>{parts}</div>
+      <Person />
+      <Story />
     </div>
   );
 };
--- client/src/components/about/person.js
+++ client/src/components/about/person.js
@@ -1,9 +1,24 @@
-import React, { useState } from 'react';
+import React from 'react';
 
-const Person = (name) => {
+const Person = () => {
+  const names = [
+    'Yi Chen Zhu',
+    'Cedric Mosdell',
+    'Charles Petchsy',
+    'Harrison Fok',
+    'Allan C',
+    'Avi Patel',
+    'Bo Dan',
+    'David Tan',
+    'Hassan Azmi',
+    'Alex',
+    'Shuyi',
+  ];
+  const listItems = names.map((name) => <p key={name.toString()}>{name}</p>);
   return (
     <div>
-      <h1>{name}</h1>
+      <h3>Meet the Team!</h3>
+      <div>{listItems}</div>
     </div>
   );
 };
