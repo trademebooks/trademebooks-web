5376f5d88b881c1dde6a78331945e0b36a833219
Yichen Zhu <yichenzhu1337@gmail.com>
2020-09-26 22:25:16 -0400

    fix: took out some unused actions.

--- client/src/actions/bookstore.js
+++ client/src/actions/bookstore.js
@@ -1,22 +1,18 @@
 import api from '../utils/api';
 
 import {
-  GET_PROFILE,
-  PROFILE_ERROR,
+  GET_PROFILE
 } from './types';
 
 export const getBookstoreByUsername = (username) => async dispatch => {
   try {
-    const res = await api.get(`/profile/user/${userId}`);
+    const res = await api.get(`/bookstores/${username}`);
 
     dispatch({
       type: GET_PROFILE,
       payload: res.data
     });
-  } catch (err) {
-    dispatch({
-      type: PROFILE_ERROR,
-      payload: { msg: err.response.statusText, status: err.response.status }
-    });
+  } catch (error) {
+    console.log({ erro })
   }
 };
