349024da62efe475ae4294f0817f8eed788df6e5
HarrisonFok <pak.fok@mail.utoronto.ca>
2020-09-29 00:02:26 -0400

    Trying to dynamically add all the stuff for one person, but can't find image

--- client/src/components/about/index.js
+++ client/src/components/about/index.js
@@ -4,24 +4,29 @@ import Person from './person';
 import Story from './story';
 import PersonBlock from './personBlock';
 
+import './story.css';
+
 const About = () => {
   const names = {
-    'Yi Chen Zhu': 'I am a genius',
-    'Cedric Mosdell': 'Me too',
-    'Charles Petchsy': 'Me too',
-    'Harrison Fok': 'Not sure',
-    'Allan C': 'Me too',
-    'Avi Patel': 'Me too',
-    'Bo Dan': 'Me too',
-    'David Tan': 'Me too',
-    'Hassan Azmi': 'Me too',
-    'Alex': 'Me too',
-    'Shuyi': 'Me too',
+    'Yi Chen Zhu': ['Yi Chen is a 3rd year Software Engineering student at the University of Toronto. His main role was to code Cedric’s designs and make them come to life on the internet. This is his first time creating a site this big from scratch and it took some balls to do so! Yi Chen specializes in back-end development where he programs the magic that happens whenever a button is clicked or an account is created.'],
+    'Cedric Mosdell': ['Cedric is a 3rd year Civil Engineering student at McMaster and loves eating hamburgers. He experienced the hassles of finding his textbooks when he was in first year, so he provided the necessary insight for making MUSBE an online tool tailored to students. Cedric has a background of doing art which had led him to becoming a part-time graphics designer and creating what MUSBE looks like right now. MUSBE was also his first site he has ever designed in his life! Woot!'],
+    // 'Charles Petchsy': 'Me too',
+    // 'Harrison Fok': 'Not sure',
+    // 'Allan C': 'Me too',
+    // 'Avi Patel': 'Me too',
+    // 'Bo Dan': 'Me too',
+    // 'David Tan': 'Me too',
+    // 'Hassan Azmi': 'Me too',
+    // 'Alex': 'Me too',
+    // 'Shuyi': 'Me too',
   };
-  const blocks = Object.keys(names).map(function (key) {
+  const imagePaths = ['yi_chen.jpg', 'cedric.jpg']
+
+  const blocks = Object.keys(names).map(function (key, i) {
     return (
-      <div>
+      <div className="centerTexts">
         <p>{key}</p>
+        <img src={imagePaths[i]}/>
         <p>{names[key]}</p>
       </div>
     );
