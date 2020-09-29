7420df5a0135e77fa90fc6d148ebf2dee0f7c49e
HarrisonFok <pak.fok@mail.utoronto.ca>
2020-09-29 00:39:06 -0400

    don't know why images only work with hard-coded paths

--- client/src/components/about/index.js
+++ client/src/components/about/index.js
@@ -8,8 +8,8 @@ import './story.css';
 
 const About = () => {
   const names = {
-    'Yi Chen Zhu': ['Yi Chen is a 3rd year Software Engineering student at the University of Toronto. His main role was to code Cedric’s designs and make them come to life on the internet. This is his first time creating a site this big from scratch and it took some balls to do so! Yi Chen specializes in back-end development where he programs the magic that happens whenever a button is clicked or an account is created.'],
-    'Cedric Mosdell': ['Cedric is a 3rd year Civil Engineering student at McMaster and loves eating hamburgers. He experienced the hassles of finding his textbooks when he was in first year, so he provided the necessary insight for making MUSBE an online tool tailored to students. Cedric has a background of doing art which had led him to becoming a part-time graphics designer and creating what MUSBE looks like right now. MUSBE was also his first site he has ever designed in his life! Woot!'],
+    'Yi Chen Zhu': 'Yi Chen is a 3rd year Software Engineering student at the University of Toronto. His main role was to code Cedric’s designs and make them come to life on the internet. This is his first time creating a site this big from scratch and it took some balls to do so! Yi Chen specializes in back-end development where he programs the magic that happens whenever a button is clicked or an account is created.',
+    'Cedric Mosdell': 'Cedric is a 3rd year Civil Engineering student at McMaster and loves eating hamburgers. He experienced the hassles of finding his textbooks when he was in first year, so he provided the necessary insight for making MUSBE an online tool tailored to students. Cedric has a background of doing art which had led him to becoming a part-time graphics designer and creating what MUSBE looks like right now. MUSBE was also his first site he has ever designed in his life! Woot!',
     // 'Charles Petchsy': 'Me too',
     // 'Harrison Fok': 'Not sure',
     // 'Allan C': 'Me too',
@@ -20,13 +20,14 @@ const About = () => {
     // 'Alex': 'Me too',
     // 'Shuyi': 'Me too',
   };
-  const imagePaths = ['yi_chen.jpg', 'cedric.jpg']
+  const imagePaths = ["./yi_chen.jpg", "./cedric.jpg"]
 
   const blocks = Object.keys(names).map(function (key, i) {
+    // console.log(imagePaths[i])
     return (
-      <div className="centerTexts">
+      <div className="centerTexts" key={i}>
         <p>{key}</p>
-        <img src={imagePaths[i]}/>
+        <img src={require("./yi_chen.jpg")} width="300" height="300"/>
         <p>{names[key]}</p>
       </div>
     );
