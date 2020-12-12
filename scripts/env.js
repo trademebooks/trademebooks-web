2b5901933262e3e6c5a2b7145fe0342e25d2b091
Yichen Zhu <yichenzhu1337@gmail.com>
2020-12-12 12:36:07 -0500

    fix: circleci test 6 - add mongouri

--- scripts/env.js
+++ scripts/env.js
@@ -1,3 +1,7 @@
+console.log('Node env:', process.env.NODE_ENV)
+console.log('Node env equality:', process.env.NODE_ENV === 'ci')
+console.log('Node env MONGO_URI:', process.env.MONGO_URI)
+
 const config = require('../api/config')
 
-console.log('env:', config)
+console.log('config:', config)
