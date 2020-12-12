6d304ba571b58c8b7a666c271b5b4e2c7c3f17eb
Yichen Zhu <yichenzhu1337@gmail.com>
2020-12-12 13:30:46 -0500

    fix: circleci test 9 - full build

--- scripts/env.js
+++ scripts/env.js
@@ -1,7 +1,3 @@
-console.log('Node env:', process.env.NODE_ENV)
-console.log('Node env equality:', process.env.NODE_ENV === 'ci')
-console.log('Node env MONGO_URI:', process.env.MONGO_URI)
-
 const config = require('../api/config')
 
 console.log('config:', config)
