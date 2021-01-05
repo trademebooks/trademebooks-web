f857aa984344c0cb39c690b21a2809e1f631fde0
HarrisonFok <pak.fok@mail.utoronto.ca>
2021-01-05 14:27:18 -0500

    Misunderstood, now the header is in the right place; but need to make it look good

--- client/src/components/bookstore/publicBookstore/PublicBookstore.js
+++ client/src/components/bookstore/publicBookstore/PublicBookstore.js
@@ -11,24 +11,38 @@ const PublicBookstore = ({ match, books, getBookstoreByUsername }) => {
 
   return (
     <>
-      <div className="header-container text-center">
-        <h3 className="font-weight-bold">
-          Welcome to {match.params.username}'s Bookstore
-        </h3>
-      </div>
-      <div className="mt-4">
         <MDBContainer>
-          <MDBRow center={true}>
-            <MDBCol sm="12">
-              {books.length < 1 ? (
-                <div>There are no books in this bookstore...</div>
-              ) : (
-                <Books books={books} />
-              )}
+          <MDBRow>
+            <MDBCol>
+              <div className="header-container mt-4">
+                <h3 className="font-weight-bold">
+                  Welcome to {match.params.username}'s Bookstore
+                </h3>
+                
+                <i class="fas fa-map-marker-alt" style={{fontSize: "200%", height: "150%", color: "var(--primary-dark-color)"}}></i>
+                Located in:
+                North York, Toronto
+
+                <i class="fas fa-graduation-cap" style={{fontSize: "200%", height: "150%", color: "var(--primary-dark-color)"}}></i> 
+                Selling books for:
+                University of Toronto Scarborough
+              </div>
             </MDBCol>
           </MDBRow>
         </MDBContainer>
-      </div>
+        <div className="mt-4">
+          <MDBContainer>
+            <MDBRow center={true}>
+              <MDBCol sm="12">
+                {books.length < 1 ? (
+                  <div>There are no books in this bookstore...</div>
+                ) : (
+                  <Books books={books} />
+                )}
+              </MDBCol>
+            </MDBRow>
+          </MDBContainer>
+        </div>
     </>
   )
 }
--- client/src/components/pages/home/Jumbotron.js
+++ client/src/components/pages/home/Jumbotron.js
@@ -6,7 +6,7 @@ function Jumbotron({ onChangeSearchBooks }) {
   return (
     <>
       <section className="home-page-top-section">
-        {/* <div className="home-page-top-section__main-text">
+        <div className="home-page-top-section__main-text">
           <span>
             Buy & Sell Textbooks from Canada’s <b>35+</b> Universities and
             Colleges
@@ -16,16 +16,7 @@ function Jumbotron({ onChangeSearchBooks }) {
         <div className="home-page-top-section__small-caption mt-4">
           Buying and sell textbooks with ease and with minimal effort using the
           latest technology!
-        </div> */}
-
-        <h2>Welcome to <strong style={{color: "var(--primary-dark-color)"}}>YiChen's</strong> Bookstore</h2>
-        <i class="fas fa-map-marker-alt" style={{fontSize: "200%", height: "150%", color: "var(--primary-dark-color)"}}></i>
-        Located in:
-        North York, Toronto
-
-        <i class="fas fa-graduation-cap" style={{fontSize: "200%", height: "150%", color: "var(--primary-dark-color)"}}></i> 
-        Selling books for:
-        University of Toronto Scarborough
+        </div>
 
         <div className="home-search-box">
           <form>
