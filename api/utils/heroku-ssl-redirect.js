1cd129f7f6b9f50c307738f5cd3992c8af123333
Yichen Zhu <yichenzhu1337@gmail.com>
2021-03-22 19:32:22 -0400

    refactor and fix: ssl redirects for different scenarios

--- api/utils/heroku-ssl-redirect.js
+++ api/utils/heroku-ssl-redirect.js
@@ -1,19 +1,34 @@
 /**
- * Force load with https on production environment
- * https://devcenter.heroku.com/articles/http-routing#heroku-headers
- */
+* Force load with https on production environment
+* https://devcenter.heroku.com/articles/http-routing#heroku-headers
+*/
 module.exports = function (environments, status) {
-  environments = environments || ['production']
-  status = status || 302
+  environments = environments || ['production'];
+  status = status || 302;
   return function (req, res, next) {
     if (environments.indexOf(process.env.NODE_ENV) >= 0) {
-      if (req.headers['x-forwarded-proto'] != 'https') {
-        res.redirect(status, 'https://www.' + req.hostname + req.originalUrl)
-      } else {
-        next()
+      let hasSubdomain = req.subdomains.includes('www');
+
+      if (req.headers['x-forwarded-proto'] !== 'https') {
+        if (hasSubdomain) {
+          res.redirect(status, 'https://' + req.hostname + req.originalUrl);
+        }
+        else {
+          // this scenario needs to be modified if there are any other alternate subdomains such as...
+          // [blog|app|subdomain].trademebooks.com
+          res.redirect(status, 'https://www.' + req.hostname + req.originalUrl);
+        }
       }
-    } else {
-      next()
+      else if (req.headers['x-forwarded-proto'] === 'https' && !req.subdomains.includes('www')) {
+        res.redirect(status, 'https://www.' + req.hostname + req.originalUrl);
+      }
+      else {
+        next();
+      }
     }
-  }
-}
+    else {
+      next();
+    }
+  };
+};
+
