4a504a2130ceb2469f6768b357f4e4f54d133eae
Yichen Zhu <yichenzhu1337@gmail.com>
2021-07-04 12:46:17 -0400

    feat(TMB0096): add new spinner loader for home page books

--- client/src/components/pages/home/index.js
+++ client/src/components/pages/home/index.js
@@ -5,15 +5,21 @@ import Jumbotron from './Jumbotron'
 import { getAllBooks } from '../../../actions/book'
 import MetaDecorator from '../../utils/MetaDecorator'
 import metags from '../../../config/metags'
+import Spinner from '../../layout/Spinner'
 
 const HomePage = () => {
   const [books, setBooks] = useState([])
+  const [booksIsLoading, setBooksIsLoading] = useState(true)
 
   const onChangeSearchBooks = async (event) => {
     let searchQuery = event.target.value
 
     if (searchQuery.length >= 0) {
+      setBooksIsLoading(true)
+
       const books = await getAllBooks(searchQuery, 20)
+
+      setBooksIsLoading(false)
 
       setBooks(books)
     }
@@ -21,7 +27,11 @@ const HomePage = () => {
 
   useEffect(() => {
     ;(async () => {
+      setBooksIsLoading(true)
+
       const books = await getAllBooks(undefined, 20)
+
+      setBooksIsLoading(false)
 
       setBooks(books)
     })()
@@ -39,7 +49,11 @@ const HomePage = () => {
         <MDBRow>
           <MDBCol sm="12">
             <Jumbotron onChangeSearchBooks={onChangeSearchBooks} />
-            <Books books={books} editFlag={false} />
+            {booksIsLoading ? (
+              <Spinner isLoading={false} />
+            ) : (
+              <Books books={books} editFlag={false} />
+            )}
           </MDBCol>
         </MDBRow>
       </MDBContainer>
