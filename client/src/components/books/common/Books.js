1c3dc6daad011af0245d110b4b587d478d5d8ac4
Yichen Zhu <yichenzhu1337@gmail.com>
2021-07-04 18:35:31 -0400

    fix: add no search results if the books length return is 0

--- client/src/components/books/common/Books.js
+++ client/src/components/books/common/Books.js
@@ -4,13 +4,23 @@ import PropTypes from 'prop-types'
 import Book from './Book'
 
 const Books = ({ books, editFlag }) => {
+  if (books && books.length > 0) {
+    return (
+      <>
+        <div>
+          {books &&
+            books.map((book, index) => {
+              return <Book key={book._id} book={book} editFlag={editFlag} />
+            })}
+        </div>
+      </>
+    )
+  }
+
   return (
     <>
       <div>
-        {books &&
-          books.map((book, index) => {
-            return <Book key={book._id} book={book} editFlag={editFlag} />
-          })}
+        <p>There are no results that match.</p>
       </div>
     </>
   )
